92f8535c1352f9de6bd452f6f63db2a72a54c5af
Admin <ponomarenko02041993@gmail.com>
2018-01-28 21:48:30 +0200

    new version-2

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,16 +1,17 @@
-$(document).ready(function(){
-    $('.next').click(function(){
+$(document).ready(function () {
+    $('.next').click(function () {
         var next = $('ul.pagination-list').find('li.active').removeClass('active').next();
-        next.index() < 0 &&(next = $("ul.pagination-list li:first"));
+        next.index() < 0 && (next = $("ul.pagination-list li:first"));
         next.addClass('active');
         return false;
     });
-    $('.prev').click(function(){
+    $('.prev').click(function () {
         var prev = $('ul.pagination-list').find('li.active').removeClass('active').prev();
-        prev.index() < 0 &&(prev = $("ul.pagination-list li:last"));
+        prev.index() < 0 && (prev = $("ul.pagination-list li:last"));
         prev.addClass('active');
         return false;
     });
-});
+
+})
 
 
--- src/components/WriteUsSection.js
+++ src/components/WriteUsSection.js
@@ -1,44 +1,58 @@
 import React from 'react';
-const WriteUsSection =()=>{
-    return(
-        <section className="write-us">
-            <div className="container">
-                <header className="sections-header">
-                    <h2>Write to Us</h2>
-                </header>
-                <form className="write">
-                    <div className="wrapper-input">
-                        <div className="write-name">
-                            <label htmlFor="id-name">Name</label>
-                            <input type="text" id="id-name"/>
+
+class WriteUsSection extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {value: 'services'};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    render()
+    {
+        return (
+            <section className="write-us">
+                <div className="container">
+                    <header className="sections-header">
+                        <h2>Write to Us</h2>
+                    </header>
+                    <form className="write">
+                        <div className="wrapper-input">
+                            <div className="write-name">
+                                <label htmlFor="id-name">Name</label>
+                                <input type="text" id="id-name"/>
+                            </div>
+                            <div className="write-name">
+                                <label htmlFor="id-email">Email</label>
+                                <input type="email" id="id-email"/>
+                            </div>
                         </div>
-                        <div className="write-name">
-                            <label htmlFor="id-email">Email</label>
-                            <input type="email" id="id-email"/>
+                        <div className="wrapper-input">
+                            <div className="write-name">
+                                <label htmlFor="id-phone">Phone</label>
+                                <input type="text" id="id-phone"/>
+                            </div>
+                            <div className="write-name">
+                                <select id="id-serv" value={this.state.value} onChange={this.handleChange}>
+                                    <option value="services">services</option>
+                                    <option value="services-2">services-2</option>
+                                    <option value="services-3">services-3</option>
+                                    <option value="services-4">services-4</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div className="wrapper-input">
-                        <div className="write-name">
-                            <label htmlFor="id-phone">Phone</label>
-                            <input type="text" id="id-phone"/>
+                        <div className="write-name-area">
+                            <label htmlFor="comment-id">Comment</label>
+                            <textarea id="comment-id" rows="5"></textarea>
                         </div>
-                        <div className="write-name">
-                            <label htmlFor="id-serv">Service</label>
-                            <select id="id-serv">
-                                <option></option>
-                                <option></option>
-                                <option></option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="write-name-area">
-                        <label htmlFor="comment-id">Comment</label>
-                        <textarea id="comment-id" rows="5"></textarea>
-                    </div>
-                    <button type="submit" className="btn-back submit">Submit Now</button>
-                </form>
-            </div>
-        </section>
-    );
+                        <button type="submit" className="btn-back submit">Submit Now</button>
+                    </form>
+                </div>
+            </section>
+        );
+    }
 }
 export default WriteUsSection;
